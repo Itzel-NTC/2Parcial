1d11d0f142a6ace201e56e22a3b2a04216dcc208
itzel <itzelnayeli2205@gmail.com>
2022-05-06 10:18:41 -0400

    mi segundo componente

---
+++ src/componentes/agenda2.jsx
@@ -0,0 +1,19 @@
+<div class="card">
+<div class="card_header purple">
+    <div class="tittle">
+        <div class="circle">1</div>
+        <p>Agenda/Topic</p>
+    </div>
+    <div class="id">
+        <p>SAAS-000</p>
+
+    </div>
+</div>
+<div class="card_section">
+    <p>Descripcion de la card</p>
+</div>
+<div class="card_footer">
+    <p>Mis Actualizaciones</p>
+    <p>14/07/2021</p>
+</div>
+</div>
